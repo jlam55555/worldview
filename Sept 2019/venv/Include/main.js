bc3fa14cbe18bbf269fd4426ff795e2874ff9bed
Tony Belladonna <belladon@cooper.edu>
2019-09-07 01:51:50 -0400

    Added stuff

--- Sept 2019/venv/Include/main.js
+++ Sept 2019/venv/Include/main.js
@@ -8,27 +8,20 @@ let river = canvas.getContext('2d');
 let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
 
-let x = 98, y = 70;
-
-function ElementPosition() {
-	this.landXPos = 0;
-  this.landYPos = 0;
-  this.riverXPos = 0;
-  this.riverYPos = 0;
-}
+let x = 0, y = 0;
 
 function draw() {
 
 	ctx.clearRect(0, 0, width, height);
 
 	map.fillStyle = 'green';
-	map.fillRect(0, 0, 900, 500);
+	map.fillRect(x, y, 900, 500);
 
-  river.fillStyle= 'blue';
-  river.fillRect(0, 300, 900, 100);
+  	river.fillStyle= 'blue';
+ 	river.fillRect(x,y + 300, 900, 100);
 
 	ctx.fillStyle = 'red';
-	ctx.fillRect(x, y, 15, 30);
+	ctx.fillRect(window.innerWidth/2, window.innerHeight/2, 15, 30);
 
 	requestAnimationFrame(draw);
 }
@@ -44,13 +37,13 @@ checkMapShift(x);
 
 setInterval(function() {
 	if(activeKeys.ArrowLeft)
-  	x--;
-  else if(activeKeys.ArrowRight)
   	x++;
+  else if(activeKeys.ArrowRight)
+  	x--;
   if(activeKeys.ArrowUp)
-  	y--;
-  else if(activeKeys.ArrowDown)
   	y++;
+  else if(activeKeys.ArrowDown)
+  	y--;
 }, 10);
 
 let activeKeys = {
